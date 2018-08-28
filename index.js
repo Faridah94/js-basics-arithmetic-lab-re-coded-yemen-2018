0d50832feb7c269604e4e1ee284b8aa6aa3d4313
Farida <faridah.absi@gmail.com>
2018-08-28 19:15:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,4 +14,4 @@ const randomNumber = Math.floor(Math.random()*20) + 1;
 
 const randomInteger =Math.floor(randomNumber);
 
-const randomUserID = Math.ceil(newID) ;
+const randomUserID = 1000000000 + Math.ceil(newID) ;
